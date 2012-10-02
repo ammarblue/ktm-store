3705ea8f001e47e7c23e21069431ee19412d01e0
sarachaii <sarachaii@32fa4473-4d42-6536-52c2-15efe3c206f4>
2012-10-02 17:39:33 +0000

    Add main index servlet

---
+++ trunk/src/org/ktm/stock/Index.java
@@ -0,0 +1,53 @@
+package org.ktm.stock;
+
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
+import org.ktm.servlet.AbstractServlet;
+import org.ktm.servlet.ActionForward;
+import org.ktm.stock.bean.FormBean;
+import org.ktm.tags.auth.AuthenticatorFactory;
+import org.ktm.utils.Globals;
+
+@WebServlet("/index")
+public class Index extends AbstractServlet {
+    private static final long serialVersionUID = 1L;
+
+    private final Logger      log              = Logger.getLogger(Index.class);
+
+    private String            redirectName;
+
+    public String getRedirectName() {
+        return redirectName;
+    }
+
+    @Override
+    protected ActionForward processRequest(FormBean form, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        redirectName = request.getParameter("page");
+        ActionForward action = null;
+
+        String pageType = request.getParameter("t");
+        if (pageType != null && redirectName != null) {
+            if (pageType.equals("t")) {
+                log.info("Go to page: " + redirectName);
+                action = ActionForward.getAction(this, request, redirectName);
+            }
+        }
+        if (redirectName == null) {
+            if (AuthenticatorFactory.isUserLoggedIn(request)) {
+                ServletContext context = request.getServletContext();
+                String mainPage = context.getInitParameter(Globals.MAIN_PAGE);
+                action = ActionForward.getAction(this, request, mainPage);
+            } else {
+                log.info("Redirect to user-login");
+                action = ActionForward.getAction(this, request, "index?t=t&page=login");
+            }
+        }
+        return action;
+    }
+
+}
